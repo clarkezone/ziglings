cbae8559f96eb22a4b0c607df31dcbb802b62404
James Clarke <james@clarkezone.net>
2024-02-02 07:24:16 -0800

    WIP

--- exercises/059_integers.zig
+++ exercises/059_integers.zig
@@ -20,9 +20,12 @@ const print = @import("std").debug.print;
 
 pub fn main() void {
     const zig = [_]u8{
-        0o131, // octal
-        0b1101000, // binary
-        0x66, // hex
+        'Z',
+        'i',
+        'g',
+        //        0o131, // octal
+        //        0b1101000, // binary
+        //       0x66, // hex
     };
 
     print("{s} is cool.\n", .{zig});
